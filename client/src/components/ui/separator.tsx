778fee36f54a015b5f4c9bd3999878862677652a
kaunda-a <kaunda.a@icloud.com>
2025-06-01 09:36:22 +0200

    Deploy: Trigger automated build and release

--- client/src/components/ui/separator.tsx
+++ client/src/components/ui/separator.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react'
 import * as SeparatorPrimitive from '@radix-ui/react-separator'
-import { cn } from '@/lib/utils'
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+// Inline cn function to avoid import issues during build
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 
 const Separator = React.forwardRef<
   React.ElementRef<typeof SeparatorPrimitive.Root>,
