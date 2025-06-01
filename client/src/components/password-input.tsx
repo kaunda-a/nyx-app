3818650214e6f1d33f129ad223dad9ab94c8ea2f
kaunda-a <kaunda.a@icloud.com>
2025-06-01 11:27:24 +0200

    Deploy: Trigger automated build and release

--- client/src/components/password-input.tsx
+++ client/src/components/password-input.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react'
 import { IconEye, IconEyeOff } from '@tabler/icons-react'
-import { cn } from '@/lib/utils'
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+// Inline cn function to avoid import issues during build
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 import { Button } from './ui/button'
 
 type PasswordInputProps = Omit<
