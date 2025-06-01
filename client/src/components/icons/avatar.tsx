057047888927d149f8bf4f2833248abfcfa488da
kaunda-a <kaunda.a@icloud.com>
2025-06-01 13:20:00 +0200

    Deploy: Trigger automated build and release

--- client/src/components/icons/avatar.tsx
+++ client/src/components/icons/avatar.tsx
@@ -1,5 +1,11 @@
 import { type ReactElement } from 'react'
-import { cn } from '@/lib/utils'
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+// Inline cn function to avoid import issues during build
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 
 type BaseAvatarProps = React.SVGProps<SVGSVGElement>
 type InitialsAvatarProps = BaseAvatarProps & { initials: string }
